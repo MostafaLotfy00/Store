793c08db3474ce170a1b9791ff82723596f0838c
Mostafa Lotfy <lotfymostafa800@gmail.com>
2024-05-03 08:57:38 +0300

    Template added

---
+++ resources/views/back/layout.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<!--
+This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
+<html lang="en">
+@include('back.partials.head')
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
+
+  @include('back.partials.header')
+  
+  @include('back.partials.sidebar')
+ 
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">@yield('page')</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              @section('breadcrump')
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              @show
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    @yield('content')
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+    <div class="p-3">
+      <h5>Title</h5>
+      <p>Sidebar content</p>
+    </div>
+  </aside>
+  <!-- /.control-sidebar -->
+
+  @include('back.partials.footer')
+</div>
+<!-- ./wrapper -->
+
+  @include('back.partials.js')
+</body>
+</html>
---
+++ resources/views/back/partials/js.blade.php
@@ -0,0 +1,9 @@
+
+<!-- REQUIRED SCRIPTS -->
+
+<!-- jQuery -->
+<script src="{{ asset('back-assets') }}/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="{{ asset('back-assets') }}/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="{{ asset('back-assets') }}/dist/js/adminlte.min.js"></script>
